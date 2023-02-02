7d43c5445bbb3b31bc9990c0d8f20325182d879c
Ednover <56236636+Ednover@users.noreply.github.com>
2023-02-02 14:28:35 -0600

    Add files via upload

---
+++ pokescript.sh
@@ -0,0 +1,13 @@
+pokeName=$1
+
+url="https://pokeapi.co/api/v2/pokemon/$pokeName"
+
+id=$(curl "$url" | jq .id)
+name=$(curl "$url" | jq .name)
+weight=$(curl "$url" | jq .weight)
+height=$(curl "$url" | jq .height)
+order=$(curl "$url" | jq .order)
+
+clear
+
+echo "id=$id, name=$name, weight=$weight, height=$height, order=$order"
